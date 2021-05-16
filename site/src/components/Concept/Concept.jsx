ad2a041a80527d3cc1e7c9044bea2272f834f66c
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-05-16 23:25:54 +0900

    不要なimportを削除

--- site/src/components/Concept/Concept.jsx
+++ site/src/components/Concept/Concept.jsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Container, Row, Col, Tab } from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
 
 const Concept = () => (
   <div id="concept">
